cb48e51953ba2a492f5a1c57ac4510f8b8a98c1a
ShixiongQi <qishixiong19931009@gmail.com>
2022-06-05 12:11:56 -0500

    Load generator for testing shared memory communication

--- locustfile.py
+++ locustfile.py
@@ -29,46 +29,50 @@ products = [
     'OLJCESPC7Z']
 
 def index(l):
-    l.client.get("/", headers={"host": "kn-frontend.default.example.com"})
+    l.client.get("/1")
 
 def setCurrency(l):
     currencies = ['EUR', 'USD', 'JPY', 'CAD']
-    l.client.post("/setCurrency",
-        {'currency_code': random.choice(currencies)}, headers={"host": "kn-frontend.default.example.com"})
+    cur = random.choice(currencies)
+    l.client.post("/1/setCurrency" + "?currency_code=" + cur)
+    # l.client.post("/1/setCurrency",
+    #     {'currency_code': random.choice(currencies)})
 
 def browseProduct(l):
-    l.client.get("/product/" + random.choice(products), headers={"host": "kn-frontend.default.example.com"})
+    l.client.get("/1/product?" + random.choice(products))
 
 def viewCart(l):
-    l.client.get("/cart", headers={"host": "kn-frontend.default.example.com"})
+    l.client.get("/1/cart")
 
 def addToCart(l):
     product = random.choice(products)
-    l.client.get("/product/" + product, headers={"host": "kn-frontend.default.example.com"})
-    l.client.post("/cart", {
-        'product_id': product,
-        'quantity': random.choice([1,2,3,4,5,10])}, headers={"host": "kn-frontend.default.example.com"})
+    l.client.get("/1/product?" + product)
+    l.client.post("/1/cart" + "?" + "product_id=" + product + "&" + "quantity=" + str(random.choice([1,2,3,4,5,10])))
+    # l.client.post("/1/cart", {
+    #     'product_id': product,
+    #     'quantity': random.choice([1,2,3,4,5,10])})
 
 def checkout(l):
     addToCart(l)
-    l.client.post("/cart/checkout", {
-        'email': 'someone@example.com',
-        'street_address': '1600 Amphitheatre Parkway',
-        'zip_code': '94043',
-        'city': 'Mountain View',
-        'state': 'CA',
-        'country': 'United States',
-        'credit_card_number': '4432801561520454',
-        'credit_card_expiration_month': '1',
-        'credit_card_expiration_year': '2039',
-        'credit_card_cvv': '672',
-    }, headers={"host": "kn-frontend.default.example.com"})
+    l.client.post("/1/cart/checkout" + "?email=someone@example.com&street_address=1600-Amphitheatre-Parkway&zip_code=94043&city=Mountain-View&state=CA&country=United-States&credit_card_number=4432801561520454&credit_card_expiration_month=1&credit_card_expiration_year=2039&credit_card_cvv=672")
+    # l.client.post("/1/cart/checkout", {
+    #     'email': 'someone@example.com',
+    #     'street_address': '1600 Amphitheatre Parkway',
+    #     'zip_code': '94043',
+    #     'city': 'Mountain View',
+    #     'state': 'CA',
+    #     'country': 'United States',
+    #     'credit_card_number': '4432801561520454',
+    #     'credit_card_expiration_month': '1',
+    #     'credit_card_expiration_year': '2039',
+    #     'credit_card_cvv': '672',
+    # })
 
 class UserBehavior(TaskSet):
 
     def on_start(self):
         index(self)
-
+    # tasks = {index: 1}
     tasks = {index: 1,
         setCurrency: 2,
         browseProduct: 10,
